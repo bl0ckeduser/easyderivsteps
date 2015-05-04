0fe12a81a3ab3246ac19ff454ad034cf94235330
bl0ckeduser <bl0ckedusersoft@gmail.com>
2015-05-04 14:43:24 -0400

    descgen: trace optimizer

--- descgen.sh
+++ descgen.sh
@@ -1,1 +1,1 @@
-cc -DDESC_GEN -g `ls *.c | grep -v jsmain` debugmain/*.c -Demscripten_run_script=puts
+cc -DOPT_DEBUG -DDESC_GEN -g `ls *.c | grep -v jsmain` debugmain/*.c -Demscripten_run_script=puts
