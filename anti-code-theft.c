9b2893f70608b8494a4404e873dfcb45264e8627
bl0ckeduser <bl0ckedusersoft@gmail.com>
2016-09-24 15:14:04 -0400

    update date to 2016

--- anti-code-theft.c
+++ anti-code-theft.c
@@ -1,5 +1,5 @@
 /* check for theft-of-code... :| */
 
 void derp(){
-	emscripten_run_script("if (escape(document.getElementById('derp').innerHTML + document.getElementById('derp').style.display) != '%3Cfont%20size%3D%22-1%22%3E%28C%29%202013-2015%2C%20bl0ckeduser%3C/font%3E') { document.write(''); } ;");
+	emscripten_run_script("if (escape(document.getElementById('derp').innerHTML + document.getElementById('derp').style.display) != '%3Cfont%20size%3D%22-1%22%3E%28C%29%202013-2016%2C%20bl0ckeduser%3C/font%3E') { document.write(''); } ;");
 }
