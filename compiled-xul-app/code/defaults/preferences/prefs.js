c52dcd259dfc71d31b61d3e7bd916bc1da820cde
bl0ckeduser <bl0ckedusersoft@gmail.com>
2016-09-24 15:31:48 -0400

    Add compiled app directory to git

---
+++ compiled-xul-app/code/defaults/preferences/prefs.js
@@ -0,0 +1,9 @@
+pref("toolkit.defaultChromeURI", "chrome://webapp/content/main.xul");
+
+/* debugging */
+pref("browser.dom.window.dump.enabled", true);
+pref("javascript.options.showInConsole", true);
+pref("javascript.options.strict", true);
+pref("nglayout.debug.disable_xul_cache", true);
+pref("nglayout.debug.disable_xul_fastload", true);
+
---
+++ compiled-xul-app/linux-run.sh
@@ -0,0 +1,2 @@
+cd code
+xulrunner application.ini 
