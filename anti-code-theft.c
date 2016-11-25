512a1246e1522e81d37170c0eef6d8497e37f894
bl0ckeduser <bl0ckedusersoft@gmail.com>
2016-11-25 10:39:44 -0500

    add a note about the antitheft stuff

--- anti-code-theft.c
+++ anti-code-theft.c
@@ -1,5 +1,11 @@
 /* check for theft-of-code... :| */
 
+/*
+ * 2016 note, this is no longer useful since this product was formerly proprietary
+ * but no longer is, now the source code is available freely online,
+ * but I didn't bother removing the code.
+ */
+
 void derp(){
 	emscripten_run_script("if (escape(document.getElementById('derp').innerHTML + document.getElementById('derp').style.display) != '%3Cfont%20size%3D%22-1%22%3E%28C%29%202013-2016%2C%20bl0ckeduser%3C/font%3E') { document.write(''); } ;");
 }
